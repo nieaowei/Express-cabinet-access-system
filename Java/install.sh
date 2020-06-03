7def1da174e39f2257dcb2454011c60ef1a2c45e
nieaowei <nieaowei@gmail.com>
2020-06-03 20:55:38 +0800

    解决脚本路径问题。

--- Java/install.sh
+++ Java/install.sh
@@ -40,7 +40,7 @@ else
 	exit 3
 fi
 
-if docker run --name --net=host rabbitmq -d rabbitmq
+if docker run --name rabbitmq --net=host rabbitmq -d rabbitmq
 then
 	echo "The image of rabbitmq is running."
 else
